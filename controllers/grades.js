2ba7350b710613553b96422d01e60abed2dd55dc
Hom Dahal <homdahal997@gmail.com>
2024-06-01 06:55:28 -0400

    Added CRUD operations for grades The code changes include adding CRUD operations for grades in the `grades.js` controller file and updating the corresponding routes in the `grades.js` file. This allows for creating, retrieving, updating, and deleting grades.

--- controllers/grades.js
+++ controllers/grades.js
@@ -2,37 +2,62 @@ const Grade = require('../models/grade');
 
 module.exports = {
     createGrade,
+    getGrades,
+    getGradeById,
+    updateGrade,
+    deleteGrade,
 }
-
 async function createGrade(req, res) {
     try {
-        const { scores, class_id, learner_id } = req.body;
-
-        // Check if all required fields are present
-        if (!scores || !class_id || !learner_id) {
-            return res.status(400).json({ error: 'Missing required fields' });
-        }
-
-        // Check if scores is an array
-        if (!Array.isArray(scores)) {
-            return res.status(400).json({ error: 'Scores must be an array' });
-        }
-
-        // Check if each score has required fields
-        for (let score of scores) {
-            if (!score.type || !score.score) {
-                return res.status(400).json({ error: 'Each score must have a type and a score like ("type":"exam" and "score":20) ' });
-            }
-        }
-
         const grade = new Grade(req.body);
         await grade.save();
         res.status(200).json(grade);
     } catch (error) {
-        console.log(error);
-        if (error.code === 11000) {
-            return res.status(400).json({ error: 'Duplicate key error: a document with these values already exists.' });
-        }
-        res.status(400).json(error);
+        res.status(400).json(err);
+    }
+}
+
+async function getGrades(req, res) {
+    try {
+        const grades = await Grade.find({});
+
+        res.status(200).json(grades);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+}
+
+// Get a single Grade by ID
+async function getGradeById(req, res) {
+    try {
+        const grade = await Grade.findById(req.params.id);
+
+        res.status(200).json(grade);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+}
+
+async function updateGrade(req, res) {
+    try {
+        const updatedGrade = await Grade.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+        });
+
+        res.status(200).json(updatedGrade);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+}
+
+async function deleteGrade(req, res) {
+    try {
+        await Grade.findByIdAndDelete(req.params.id);
+
+        res.status(200).json({
+            message: 'Successfully Deleted the Grade',
+        });
+    } catch (err) {
+        res.status(400).send(err);
     }
 }
--- routes/grades.js
+++ routes/grades.js
@@ -4,5 +4,9 @@ const gradesCtrl = require('../controllers/grades.js');
 const router = Router();
 
 router.post('/', gradesCtrl.createGrade);
+router.get('/', gradesCtrl.getGrades);
+router.get('/:id', gradesCtrl.getGradeById);
+router.patch('/', gradesCtrl.updateGrade);
+router.delete('/', gradesCtrl.deleteGrade);
 
 module.exports = router;
